8eab1785a9d08fb2fff45bdc279f1eddc42a574b
Jayanth Peetla <sadhanaiosapp@gmail.com>
2024-04-19 14:07:11 -0400

    Initial Commit

---
+++ Sadhana/SadhanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SadhanaApp.swift
+//  Sadhana
+//
+//  Created by Sadhana on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SadhanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
