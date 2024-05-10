d64207082fe6300611609cf4a5bbcaa5dafbfb1f
Jayanth Peetla <sadhanaiosapp@gmail.com>
2024-05-10 12:03:54 -0400

    Created view for friend requests

---
+++ Sadhana/Components/FriendRequestView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendRequestView.swift
+//  Sadhana
+//
+//  Created by Sadhana on 5/10/24.
+//
+
+import SwiftUI
+
+struct FriendRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendRequestView()
+}
---
+++ Sadhana/Core/Profile/SettingsTab/FriendRequestsView.swift
@@ -0,0 +1,8 @@
+//
+//  FriendRequestsView.swift
+//  Sadhana
+//
+//  Created by Sadhana on 5/10/24.
+//
+
+import Foundation
